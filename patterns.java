1d6bd2c10151e757e3808d9a6564f141baa35b89
Sowjanyadusanapudi <sowjanyadusanapudi531@gmail.com>
2025-06-19 11:37:39 +0530

    Update patterns.java

--- patterns.java
+++ patterns.java
@@ -173,31 +173,6 @@ output:       *
             * * *
            * * * *
           * * * * *       */
-/*11. public class patterns
-         { 
-          public static void main (String args[])
-         { 
-         int a=5,n=0;
-         for(int i=1; i<=a;i++,n=0)
-         {
-         for(int j=1; j<=a-i;j++)
-        { 
-        System.out.print("  "); 
-        }
-       while (n!=2*i-1) 
-       {
-       System.out.print("* ");
-       ++n;
-       } 
-       System.out.println();
-       }
-       }
-       } 
-output:          *
-               * * *
-             * * * * *
-           * * * * * * *
-         * * * * * * * * *      */
 /*9.public class patterns
        { 
        public static void main (String args[])
@@ -239,6 +214,239 @@ output:   I
           IND   
           INDI
           INDIA  */
+/*11. public class patterns
+         { 
+          public static void main (String args[])
+         { 
+         int a=5,n=0;
+         for(int i=1; i<=a;i++,n=0)
+         {
+         for(int j=1; j<=a-i;j++)
+        { 
+        System.out.print("  "); 
+        }
+       while (n!=2*i-1) 
+       {
+       System.out.print("* ");
+       ++n;
+       } 
+       System.out.println();
+       }
+       }
+       } 
+output:          *
+               * * *
+             * * * * *
+           * * * * * * *
+         * * * * * * * * *      */
+/*12. public class patterns
+          {
+         public static void main(String[] args)
+          {
+          int n=4;
+        for(int i=1;i<=n;i++)
+    {
+    for(int j=i;j<n;j++)
+    {
+    System.out.print("  ");
+    }
+    for(int j=1;j<=(2*i-1);j++)
+    {
+    System.out.print("* ");
+    }
+    System.out.println();
+    }
+    for(int i=n-1;i>=1;i--)
+    {
+    for(int j=n;j>i;j--)
+    {
+    System.out.print("  ");
+    }
+    for(int j=1;j<=(2*i-1);j++)
+    {
+    System.out.print("* ");
+    }
+    System.out.println("  ");
+    }
+    }
+    }
+output:         *
+              * * *
+            * * * * *
+          * * * * * * *
+            * * * * *
+              * * *
+                *             */
+/*13. public class patterns
+ {
+ public static void main(String[] args)
+ {
+   int n = 4;
+for (int i = 0; i < n; i++)
+ {
+    for (int j = 0; j < n - i - 1; j++)
+     System.out.print(" ");
+    System.out.print("*");
+     if (i != 0) 
+{
+    for (int j = 0; j < 2 * i - 1; j++)
+   System.out.print(" ");
+   System.out.print("*");
+   }
+   System.out.println();
+  }
+for (int i = n - 2; i >= 0; i--) 
+{
+ for (int j = 0; j < n - i - 1; j++)
+   System.out.print(" ");
+   System.out.print("*");
+    if (i != 0)
+ {
+    for (int j = 0; j < 2 * i - 1; j++)
+    System.out.print(" ");
+    System.out.print("*");
+ }
+  System.out.println();
+        }
+    }
+}
+output:         *
+               * *
+              *   *
+             *     *
+              *   *
+               * *
+                *           */
+/*14. public class patterns
+{
+public static void main(String[] args) 
+{
+int n = 5;
+ for (int i = 1; i <= n; i++) 
+{
+for (int j = 1; j <= n; j++) 
+{
+ if (i == 1 || i == n || j == 1 || j == n)
+System.out.print("*  ");
+ else
+ System.out.print("   ");
+}
+System.out.println();
+}
+}
+}
+output:        *  *  *  *  *
+               *           *
+               *           *
+               *           *
+               *  *  *  *  *       */
+/*15. public class patterns
+{
+public static void main(String[] args)
+ {
+ int n = 7;
+ int mid = n / 2;
+ for (int i = 0; i < n; i++)  
+{
+  for (int j = 0; j < n; j++)
+ {
+  if (i == mid || j == mid)
+ {
+  System.out.print("*");
+  } 
+else
+ {
+ System.out.print(" ");
+ }
+ }
+System.out.println();
+ }
+}
+}
+output:       *
+              *
+              *
+           *******
+              *
+              *
+              *     */
+/*16.public class patterns
+{
+ public static void main(String[] args)
+{
+ int n = 7;
+ for (int i = 1; i <= n; i++)
+ {
+ for (int j = 1; j <= n; j++)
+{
+ if (j == i || j == n - i + 1)
+System.out.print("*");
+else
+System.out.print(" ");
+}
+System.out.println();
+ }
+}
+}
+output:       *     *
+               *   *
+                * *
+                 *
+                * *
+               *   *
+              *     *      */
+/*17.public class patterns
+{
+public static void main(String[] args)
+ {
+int n = 7;
+for (int i = 1; i <= n; i++)
+ {
+for (int j = 1; j <= n; j++)
+ {
+if (i == 1||i == n||i==(n+1)/2 ||j ==1||j==n||j==(n + 1)/2)
+System.out.print("*  ");
+else
+System.out.print("   ");
+}
+System.out.println();
+}
+}
+}
+output:        *  *  *  *  *  * *
+               *        *       *
+               *        *       *
+               *  *  *  *  *  * *
+               *        *       *
+               *        *       *
+               *  *  *  *  *  * *     */
+/*18. public class patterns
+{
+ public static void main(String[] args) 
+{
+int n = 7;
+for (int i = n; i >= 1; i--)
+ {
+ for (int j = 1; j <= i; j++) 
+{
+ if (j == 1 || j == i || i == n) 
+{
+System.out.print("*");
+} else {
+System.out.print(" ");
+ }
+}
+System.out.println();
+}
+}
+}   
+output:        *******
+               *    *
+               *   *
+               *  *
+               * *
+               **
+               *     */
 /*19. public class patterns
           { 
           public static void main (String args[])
@@ -259,3 +467,35 @@ output:     0
             ***3
             ****4
             *****5      */
+/*20. public class patterns
+{
+ public static void main(String[] args)
+ {
+ int n = 7;
+for (int i = 1; i <= n; i++)
+ {
+for (int j = 1; j <= n; j++)
+{
+if (i == 1)
+ {
+System.out.print("* ");
+}
+else if (j == 1 || j == i) 
+{
+System.out.print("* ");
+}
+else 
+{
+ System.out.print(" ");
+}
+ }
+System.out.println();
+}
+}
+}output:       * * * * * * *
+               * *
+               *  *
+               *   *
+               *    *
+               *     *
+               *      *     */
